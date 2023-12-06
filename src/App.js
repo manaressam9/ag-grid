be1dd8c85bc69d714bf43db44cadd7b8032e1dc6
manaressam9 <manareldredy@gmail.com>
2023-12-06 16:14:02 +0200

    infinite row model with virtual scroll worked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+import React, { useCallback, useMemo, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
 function App() {
+  const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+
+  const [columnDefs, setColumnDefs] = useState([
+    // this row shows the row index, doesn't use any data from the row
+    {
+      headerName: 'ID',
+      maxWidth: 100,
+      // it is important to have node.id here, so that when the id changes (which happens
+      // when the row is loaded) then the cell is refreshed.
+      valueGetter: 'node.id',
+      cellRenderer: (props) => {
+        if (props.value !== undefined) {
+          return props.value;
+        } else {
+          return (
+            <img src="https://www.ag-grid.com/example-assets/loading.gif" />
+          );
+        }
+      },
+    },
+    { field: 'athlete', minWidth: 150 },
+    { field: 'age' },
+    { field: 'country', minWidth: 150 },
+    { field: 'year' },
+    { field: 'date', minWidth: 150 },
+    { field: 'sport', minWidth: 150 },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      flex: 1,
+      minWidth: 100,
+      sortable: false,
+    };
+  }, []);
+
+  const onGridReady = useCallback((params) => {
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => {
+        const dataSource = {
+          rowCount: undefined,
+          getRows: (params) => {
+            console.log(
+              'asking for ' + params.startRow + ' to ' + params.endRow
+            );
+            // At this point in your code, you would call the server.
+            // To make the demo look real, wait for 500ms before returning
+            setTimeout(function () {
+              // take a slice of the total rows
+              const rowsThisPage = data.slice(params.startRow, params.endRow);
+              // if on or after the last page, work out the last row.
+              let lastRow = -1;
+              if (data.length <= params.endRow) {
+                lastRow = data.length;
+              }
+              // call the success callback
+              params.successCallback(rowsThisPage, lastRow);
+            }, 500);
+          },
+        };
+        params.api.setGridOption('datasource', dataSource);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      <div
+        style={gridStyle}
+        className={
+          "ag-theme-quartz-dark"
+        }
+      >
+        <AgGridReact
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          rowBuffer={0}
+          rowSelection={'multiple'}
+          rowModelType={'infinite'}
+          cacheBlockSize={100}
+          cacheOverflowSize={2}
+          maxConcurrentDatasourceRequests={1}
+          infiniteInitialRowCount={1000}
+          maxBlocksInCache={10}
+          onGridReady={onGridReady}
+        />
+      </div>
     </div>
   );
-}
-
+};
 export default App;
